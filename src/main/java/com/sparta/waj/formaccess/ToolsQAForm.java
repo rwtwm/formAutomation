628b7418c0b12cbb2dab01a75dc3146ccf65609b
Waine James <wjames@spartaglobal.com>
2019-04-30 22:21:14 +0100

    Set up crawler and value returner for radio buttons.

--- src/main/java/com/sparta/waj/formaccess/ToolsQAForm.java
+++ src/main/java/com/sparta/waj/formaccess/ToolsQAForm.java
@@ -2,6 +2,9 @@ package com.sparta.waj.formaccess;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ToolsQAForm
 {
@@ -10,6 +13,10 @@ public class ToolsQAForm
     private final String PAGE_URL = "https://www.toolsqa.com/automation-practice-form";
     private final By halfLink = By.partialLinkText("Partial Link Test");
     private final By fullLink = By.linkText("Link Test");
+    private final By expButtons = By.name("exp");
+    private final By sexButtons = By.name("sex");
+    private final By continents = By.id("continents");
+
 
 
 
@@ -23,9 +30,18 @@ public class ToolsQAForm
         driver.navigate().to(PAGE_URL);
     }
 
+    /**
+     * Gets the link target of the half link.
+     * @return
+     */
     public String getHalfLinkTarget()
     {
         return driver.findElement(halfLink).getAttribute("href");
+    }
+
+    public List<WebElement> getExperience()
+    {
+        return driver.findElements(expButtons);
     }
 
 
--- src/main/java/com/sparta/waj/formaccess/elementtraversal/RadioButtonTraverse.java
+++ src/main/java/com/sparta/waj/formaccess/elementtraversal/RadioButtonTraverse.java
@@ -1,5 +1,35 @@
 package com.sparta.waj.formaccess.elementtraversal;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class RadioButtonTraverse
 {
+    List<WebElement> webElements;
+
+    public RadioButtonTraverse(List<WebElement> webElements)
+    {
+        this.webElements = webElements;
+    }
+
+    public WebElement getTopButton()
+    {
+        return webElements.get(webElements.size()-1);
+    }
+
+    public WebElement getFirstButton()
+    {
+        return webElements.get(0);
+    }
+
+    public WebElement getNthButton(int i)
+    {
+        return webElements.get(i);
+    }
+
+    public String getButtonValue(WebElement element)
+    {
+        return element.getAttribute("value");
+    }
 }
